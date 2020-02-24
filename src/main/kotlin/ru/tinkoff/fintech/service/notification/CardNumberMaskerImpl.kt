bfed02c8ae452cdf63e09a7f134f1a929264c2ec
Maksim Anikin <animax17@gmail.com>
2020-02-24 12:35:25 +0300

    Implement card mask function. Implement notification message generation.

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,11 @@
 package ru.tinkoff.fintech.service.notification
 
-class CardNumberMaskerImpl: CardNumberMasker {
+class CardNumberMaskerImpl : CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val lastIndex = getLastIndex(end, cardNumber.length)
+        return cardNumber.replaceRange(start, lastIndex, maskChar.toString().repeat(lastIndex - start))
     }
+
+    private fun getLastIndex(end: Int, length: Int): Int = if (end > length) length else end
 }
